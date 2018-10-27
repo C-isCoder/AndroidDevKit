fa61c46282acadb7ad69fe2225cf54704c3739a0
iCong <CisaCoder@gmail.com>
2018-10-27 18:33:18 +0800

    update

--- push/src/main/java/com/codingapi/android/library/push/MainActivity.java
+++ push/src/main/java/com/codingapi/android/library/push/MainActivity.java
@@ -1,19 +1,39 @@
 package com.codingapi.android.library.push;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 import com.codingapi.android.library.push.server.SocketService;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvText = findViewById(R.id.text);
+        registerReceiver(mReceiver, new IntentFilter(SocketService.MESSAGE));
         SocketService.start(this);
     }
 
-    @Override public void onBackPressed() {
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String ole = tvText.getText().toString();
+            tvText.setText(ole + intent.getStringExtra(SocketService.MESSAGE));
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
         super.onBackPressed();
         SocketService.stop(this);
         System.exit(1);
--- push/src/main/java/com/codingapi/android/library/push/server/SocketService.java
+++ push/src/main/java/com/codingapi/android/library/push/server/SocketService.java
@@ -1,18 +1,28 @@
 package com.codingapi.android.library.push.server;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
 import com.codingapi.android.library.push.R;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -21,15 +31,19 @@ import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import static android.support.v4.app.NotificationCompat.PRIORITY_MIN;
+
 public class SocketService extends Service {
-    private static final int NOTICE_ID = 1024;
     private static final String TAG = SocketService.class.getSimpleName();
+    public static final String MESSAGE = SocketService.class.getName();
+
+    private static final int NOTICE_ID = 1024;
     private static final String HOST = "58.87.75.118";
     private static final int PORT = 6666;
 
     private MediaPlayer mMediaPlayer;
     private Socket mSocket;
-    private InetSocketAddress mAddress;
+    private static final InetSocketAddress SOCKET_ADDRESS = new InetSocketAddress(HOST, PORT);
 
     public SocketService() {
     }
@@ -44,54 +58,70 @@ public class SocketService extends Service {
         context.stopService(intent);
     }
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
-        mAddress = new InetSocketAddress(HOST, PORT);
-        mSocket = new Socket();
         AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(AudioManager.STREAM_RING, 80, AudioManager.FLAG_PLAY_SOUND);
+        if (audioManager != null) {
+            audioManager.setStreamVolume(AudioManager.STREAM_RING, 80, AudioManager.FLAG_PLAY_SOUND);
+        }
         mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.silent);
         mMediaPlayer.setLooping(true);
-        Notification.Builder builder = new Notification.Builder(this);
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle("通知");
-        builder.setContentText("通知服务正在运行中...");
-        startForeground(NOTICE_ID, builder.build());
+        startForeground();
     }
 
-    @Override public IBinder onBind(Intent intent) {
+    @Override
+    public IBinder onBind(Intent intent) {
         return null;
     }
 
-    @Override public int onStartCommand(Intent intent, int flags, int startId) {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startPlayMusic();
         Thread thread = new Thread(runnable);
         thread.start();
         return START_STICKY;
     }
 
     private Runnable runnable = new Runnable() {
-        @Override public void run() {
+        @Override
+        public void run() {
             try {
-                startPlayMusic();
-                mSocket.setKeepAlive(true);
-                mSocket.connect(mAddress);
-                OutputStreamWriter writer = new OutputStreamWriter(mSocket.getOutputStream());
-                PrintWriter out = new PrintWriter(new BufferedWriter(writer), true);
-                out.println("hello");
-                StringBuilder sb = new StringBuilder();
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream(1024);
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                InputStream inputStream = mSocket.getInputStream();
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                    sb.append(outputStream.toString("UTF-8"));
+                while (true) {
+                    if (mSocket == null) {
+                        mSocket = new Socket();
+                        mSocket.connect(SOCKET_ADDRESS);
+                    }
+                    if (mSocket.isClosed() || !mSocket.isConnected()) {
+                        mSocket.connect(SOCKET_ADDRESS);
+                    }
+                    OutputStreamWriter writer = new OutputStreamWriter(mSocket.getOutputStream());
+                    PrintWriter out = new PrintWriter(new BufferedWriter(writer), true);
+                    out.println("client-ack:(ok)");
+                    StringBuilder sb = new StringBuilder();
+                    InputStream inputStream = mSocket.getInputStream();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+                    int value;
+                    while ((value = in.read()) != -1) {
+                        if ((char) value == '.') {
+                            break;
+                        }
+                        sb.append((char) value);
+                    }
+                    final Intent intent = new Intent(MESSAGE);
+                    intent.putExtra(MESSAGE, "<<<------" + sb.toString() + "-time: " + getTime() + "\n");
+                    sendBroadcast(intent);
+                    Log.i(TAG, "<<<------" + sb.toString() + "-time: " + getTime());
+                    Thread.sleep(60 * 1000 * 5);
                 }
-                Log.i(TAG, "<<<------" + sb.toString() + "-time: " + getTime());
-                out.println("client-ack:(ok)");
             } catch (UnknownHostException e) {
+                Log.e(TAG, "UnknownHostException", e);
                 e.printStackTrace();
             } catch (IOException e) {
+                Log.e(TAG, "IOException", e);
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                Log.e(TAG, "InterruptedException", e);
                 e.printStackTrace();
             }
         }
@@ -111,7 +141,8 @@ public class SocketService extends Service {
         }
     }
 
-    @Override public void onTaskRemoved(Intent rootIntent) {
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
         Log.d(TAG, "任务管理器划掉");
         stopPlayMusic();
@@ -141,5 +172,39 @@ public class SocketService extends Service {
 
     private String getTime() {
         return sData.format(System.currentTimeMillis());
+    }
+
+    private void startForeground() {
+        String channelId;
+        Notification notification;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelId = createNotificationChannel("my_service", "My Background Service");
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
+            notification = notificationBuilder.setOngoing(true)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("通知")
+                    .setContentText("通知服务正在运行中...")
+                    .setPriority(PRIORITY_MIN)
+                    .setCategory(Notification.CATEGORY_SERVICE)
+                    .build();
+        } else {
+            Notification.Builder builder = new Notification.Builder(this);
+            notification = builder.setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("通知")
+                    .setContentText("通知服务正在运行中...").build();
+        }
+        startForeground(NOTICE_ID, notification);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private String createNotificationChannel(String id, String name) {
+        NotificationChannel chan = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (service != null) {
+            service.createNotificationChannel(chan);
+        }
+        return id;
     }
 }
